7198abe0b005230f598290ef8a7f1fc7bad397eb
Ily <kingofdelusion4558@gmail.com>
2023-08-26 18:02:51 +0300

    Editor range working
    Can now change the range of a selected tower via the editor, the changes do not save unless you click the "price" button. It is not a misspelling of "purchase", but rather placeholder since that button will show  the price of the upgrades.

--- UI/Editor.gd
+++ UI/Editor.gd
@@ -1,11 +1,40 @@
 extends VBoxContainer
 
+@onready var rangebox =  $RangeContainer/RangeBox
+
+var LastSelection
+var apply = false
+
+var old_range
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	LastSelection = global.CurrentSelection
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if global.CurrentSelection != LastSelection:
+		editor_update()
+		old_range = global.CurrentSelection.range
+		LastSelection = global.CurrentSelection
+
+
+func _update_editor():
 	pass
+
+func _on_range_box_value_changed(value):
+	global.CurrentSelection.range = rangebox.value * 10
+	
+	
+	
+func editor_update():
+	rangebox.value = global.CurrentSelection.range / 10
+	if LastSelection != null:
+		if apply == false:
+			LastSelection.range = old_range
+			
+		apply = false
+
+
+func _on_button_pressed():
+	apply = true
